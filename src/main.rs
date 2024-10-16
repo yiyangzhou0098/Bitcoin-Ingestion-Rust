6d2dc39f6c2a62c2467537aa5ec2e61c324ad9d5
zyy0098 <770823669@qq.com>
2024-10-16 18:55:13 -0400

    [add]project2

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod config;
 use config::connections::{RPC_URL,RPC_PWD,RPC_USER,DB_URL};
 use services::mysql_connection::MySqlService;
 use services::bitcoin_rpc::BitcoinRpcService;
+use services::ingestion::retrieve_and_store_data;
 use server::run_server;
 use tokio::main;
 
@@ -20,7 +21,13 @@ async fn main() {
         RPC_PWD
     );
 
-    // Run the Warp server
+    // Step 1: Retrieve the latest data before starting the server
+    if let Err(e) = retrieve_and_store_data(mysql_service.clone(), bitcoin_service.clone()).await {
+        eprintln!("Error retrieving and storing initial data: {:?}", e);
+        return;
+    }
+
+    // Step 2: Run the Warp server
     run_server(mysql_service, bitcoin_service).await;
 }
 
--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,2 +1,3 @@
 pub mod bitcoin_rpc;         // Declare the bitcoin_rpc module
 pub mod mysql_connection;    // Declare the mysql_connection module
+pub mod ingestion;
