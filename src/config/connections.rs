f579ac630979fa83c3841bb8b0ce5888fa2409eb
zyy0098 <770823669@qq.com>
2024-10-24 17:52:01 -0400

    docker test

--- src/config/connections.rs
+++ src/config/connections.rs
@@ -6,4 +6,4 @@ pub const RPC_URL: &str = "http://38.42.241.37:8332";
 // MySQL connections:
 // pub const DB_USER: &str = "root";
 // pub const DB_PWD: &str = "123456789";
-pub const DB_URL: &str = "mysql://root:123456789@localhost:3306/bitcoin_data";
+pub const DB_URL: &str = "mysql://root:123456789@192.168.99.84:3306/bitcoin_data";
